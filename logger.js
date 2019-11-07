d03924b1d0ff5d62ba145508772d1c7419b81d98
vramirez <victor.ramirez@gmail.com>
2019-11-06 22:16:49 -0400

    Added excluded paths to General Site Settings. Exclusion implemented in logger-api

--- logger.js
+++ logger.js
@@ -18,7 +18,8 @@ var logger = function (app, format) {
     var synchrologTokens = [
       `[synchrolog_timestamp:${tokens['synchrolog_timestamp'](req, res)}]`,
       `[synchrolog_anonymous_id:${tokens['synchrolog_anonymous_id'](req, res)}]`,
-      `[synchrolog_user_id:${tokens['synchrolog_user_id'](req, res)}]`
+      `[synchrolog_user_id:${tokens['synchrolog_user_id'](req, res)}]`,
+      `[synchrolog_request_path:${tokens['synchrolog_request_path'](req, res)}]`,
     ].join(' ');
     return [ result, synchrologTokens ].join(' ');
   }
@@ -34,21 +35,31 @@ var logger = function (app, format) {
     exitOnError: false, // do not exit on handled exceptions
   });
 
+  const timestampMatchRegex = /\[synchrolog_timestamp:(\S*)\]/;
+  const anonIdRegex = /\[synchrolog_anonymous_id:(\S*)\]/;
+  const userIdRegex = /\[synchrolog_user_id:(\S*)\]/;
+  const reqPathRegex = /\[synchrolog_request_path:(\S*)\]/;
+
   logger.stream = {
     write: function(message, encoding) {
-      var timestampMatch = message.match(/\[synchrolog_timestamp:(\S*)\]/);
+      var timestampMatch = message.match(timestampMatchRegex);
       var timestamp = timestampMatch[1];
-      message = message.replace(/\[synchrolog_timestamp:(\S*)\]/, "");
+      message = message.replace(timestampMatchRegex, "");
 
-      var anonymousIdMatch = message.match(/\[synchrolog_anonymous_id:(\S*)\]/);
+      var anonymousIdMatch = message.match(anonIdRegex);
       var anonymousId = anonymousIdMatch[1];
       anonymousId = anonymousId === '-' ? undefined : anonymousId;
-      message = message.replace(/\[synchrolog_anonymous_id:(\S*)\]/, "");
+      message = message.replace(anonIdRegex, "");
 
-      var userIdMatch = message.match(/\[synchrolog_user_id:(\S*)\]/);
+      var userIdMatch = message.match(userIdRegex);
       var userId = userIdMatch[1];
       userId = userId === '-' ? undefined : userId;
-      message = message.replace(/\[synchrolog_user_id:(\S*)\]/, "");
+      message = message.replace(userIdRegex, "");
+
+      var requestPathMatch = message.match(reqPathRegex);
+      var requestPath = requestPathMatch[1];
+      message = message.replace(reqPathRegex, "");
+
 
       message = message.trim();
       logger.info({
@@ -60,7 +71,8 @@ var logger = function (app, format) {
           source: 'backend',
           log: {
             timestamp: timestamp,
-            message: message
+            message: message,
+            request_path: requestPath,
           }
         }
       });
@@ -73,6 +85,7 @@ var logger = function (app, format) {
   morgan.token('synchrolog_anonymous_id', function (req) { return req.synchrolog_anonymous_id || '-' });
   // if synchrolog_user_id is not set, return '-' to let the logger know it's undefined
   morgan.token('synchrolog_user_id', function (req) { return req.synchrolog_user_id || '-' });
+  morgan.token('synchrolog_request_path', function (req) { return req.originalUrl || req.url });
   app.use(morgan(formatLine, { stream: logger.stream }));
 }
 
